15468050713ce5ca3b73da1466baa9e001e8f9d2
alex <alex@targoninc.com>
2025-12-07 01:30:08 +0100

    add support for steam

--- serviceScripts/steam.js
+++ serviceScripts/steam.js
@@ -1,3 +1,16 @@
+async function getSite(url) {
+    const res = await fetch(url, {
+        credentials: "include",
+    });
+    if (res.status === 429) {
+        console.log(`Rate limit exceeded, waiting 60s...`);
+        await new Promise(resolve => setTimeout(resolve, 60 * 1000));
+        await getSite(url);
+    }
+
+    return await res.text();
+}
+
 async function getFriends() {
     const elements = document.querySelectorAll(".friend_block_v2");
     const friends = [];
@@ -9,38 +22,31 @@ async function getFriends() {
         console.log(`Processing friend ${i + 1} of ${elements.length}`);
         const element = elements[i];
         const id = element.attributes['data-steamid'].nodeValue;
+        const miniProfileId = element.attributes['data-miniprofile'].nodeValue;
         const avatarUrl = element.querySelector("img").src;
-        const profileLink = element.querySelector("a").href;
+        const profileUrl = element.querySelector("a").href;
         const name = element.querySelector(".friend_block_content").textContent
             .split("\n").at(0)
             .replace(/\*$/g, "");
 
-        const res = await fetch(profileLink, {
-            credentials: "include",
-        });
-        if (res.status === 429) {
-            console.log(`Rate limit exceeded, waiting 60s...`);
-            await new Promise(resolve => setTimeout(resolve, 60 * 1000));
-            continue;
-        }
-
-        const data = await res.text();
-        const newNode = document.createElement("div");
-        newNode.innerHTML = data;
-        const mutualsNodes = newNode.querySelector(".profile_topfriends")?.children ?? [];
+        const commonList = document.createElement("div");
+        commonList.innerHTML = await getSite(`https://steamcommunity.com/actions/PlayerList/?type=friendsincommon&target=${miniProfileId}`);
+        const mutualsNodes = commonList.querySelectorAll(".friendBlock");
 
         const friend = {
             id,
-            profileLink,
             name,
+            profileUrl,
             avatarUrl,
             mutual: [],
             source: "steam"
         };
 
-        for (const mutual of mutualsNodes) {
-            const link = mutual.querySelector("a").href;
-            friend.mutual.push(link);
+        if (mutualsNodes.length > 0) {
+            for (const mutual of mutualsNodes) {
+                const link = mutual.querySelector("a").href;
+                friend.mutual.push(link);
+            }
         }
 
         friends.push(friend);
@@ -48,12 +54,11 @@ async function getFriends() {
         await new Promise(resolve => setTimeout(resolve, 5000));
     }
 
-    console.log(friends);
     const out = {};
     for (const friend of friends) {
         const ids = [];
         for (const secondaryId of friend.mutual) {
-            const mutual = friends.find(m => m.profileLink === secondaryId);
+            const mutual = friends.find(m => m.profileUrl === secondaryId);
             if (mutual) {
                 ids.push(mutual.id);
             } else {
@@ -70,11 +75,13 @@ async function getFriends() {
 const obj = await getFriends();
 console.log(obj);
 
-const url = URL.createObjectURL(new Blob([JSON.stringify(obj)], {
+const downloadUrl = URL.createObjectURL(new Blob([JSON.stringify(obj)], {
     type: "application/json"
 }));
 Object.assign(document.createElement("a"), {
-    href: url,
+    href: downloadUrl,
     download: `steam_friends.json`
 }).click();
-URL.revokeObjectURL(url)
+setTimeout(() => {
+    URL.revokeObjectURL(downloadUrl);
+}, 2000);
